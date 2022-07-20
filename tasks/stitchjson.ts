601958e7a740848dc2415c7056e0e5ebd54531d4
Manuel Muñoz Solera <mamuso@mamuso.net>
2022-07-20 19:12:12 +0200

    Adding dumplings to the mix

--- tasks/stitchjson.ts
+++ tasks/stitchjson.ts
@@ -5,12 +5,20 @@ const filePath = 'data/data.json';
 
 let json = [];
 
+const sortJson = (a, b) => {
+  return new Date(b.date).getTime() - new Date(a.date).getTime();
+};
+
 fs.unlinkSync(filePath);
 glob('data/export/**/*.json', (error, files) => {
   files.reverse().forEach((filename) => {
     const contents = JSON.parse(fs.readFileSync(filename, 'utf8'));
     json = json.concat(contents);
   });
+
+  json.sort(sortJson);
+
   console.log(json);
+
   fs.writeFileSync(filePath, JSON.stringify(json));
 });
